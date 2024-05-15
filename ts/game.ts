d63de7efc1f24d3751f734f65dfdc09ca8fd25cd
amalea <amaleattath911@gmail.com>
2024-05-15 19:37:14 +0530

    category selection added

--- ts/game.ts
+++ ts/game.ts
@@ -43,15 +43,60 @@ const imageData = [
   },
 ];
 
-let imagesId:number[]=[];
+let imagesId: number[] = [];
+
+let categoryBtns: HTMLCollectionOf<HTMLButtonElement> =
+  document.getElementsByClassName(
+    "category-btn"
+  ) as HTMLCollectionOf<HTMLButtonElement>;
+
+for (let i = 0; i < categoryBtns.length; i++) {
+  categoryBtns[i].addEventListener("click", () => {
+    categorySelection(categoryBtns[i].value);
+  });
+}
+
+let playerOne: PlayerType = {
+  id: 1,
+  playerName: "Player 1",
+  playerStatus: true,
+  playerScore: 0,
+};
+let playerTwo: PlayerType = {
+  id: 2,
+  playerName: "Player 2",
+  playerStatus: false,
+  playerScore: 0,
+};
+
+
+const addImageEventListener = () => {
+  let imageTiles: HTMLCollectionOf<HTMLDivElement> =
+    document.getElementsByClassName(
+      "image-cards"
+    ) as HTMLCollectionOf<HTMLDivElement>;
+  for (let i = 0; i < imageTiles.length; i++) {
+    imageTiles[i].addEventListener("click", function () {
+      imageTiles[i].classList.toggle("rotated");
+      let image = imageTiles[i]!.getElementsByTagName("img");
+      let imgData = imageData.filter(
+        (img) => Number(imageTiles[i].dataset.imgId) == img.id
+      );
+      image[0].src = imgData[0].imageUrl;
+
+      selectedCards.push(imageTiles[i]);
+
+      checkCards(selectedCards);
+    });
+  }
+};
 
 const loadCards = (data: ImageTileType[], noOfCards: number) => {
-  
   const tiles = data.slice(0, noOfCards);
-  tiles.forEach(tile => {
-    imagesId.push(tile.id)
+  tiles.forEach((tile) => {
+    imagesId.push(tile.id);
   });
-  
+
   const array = [...tiles, ...tiles];
 
   shuffleArray(array);
@@ -74,74 +119,42 @@ const loadCards = (data: ImageTileType[], noOfCards: number) => {
 
     imageTileContainer?.appendChild(imageElement);
   }
+  addImageEventListener();
 };
+console.log("asd");
 
-loadCards(imageData, 8);
+// loadCards(imageData, 8);
 
-let imageTiles: HTMLCollectionOf<HTMLDivElement> =
-  document.getElementsByClassName(
-    "image-cards"
-  ) as HTMLCollectionOf<HTMLDivElement>;
-
-
-for (let i = 0; i < imageTiles.length; i++) {
-  imageTiles[i].addEventListener("click", function () {
-
-    imageTiles[i].classList.toggle('rotated');
-    let image = imageTiles[i]!.getElementsByTagName('img');
-    let imgData = imageData.filter((img)=>Number(imageTiles[i].dataset.imgId)==img.id)
-    image[0].src=(imgData[0].imageUrl)
-
-    
-    selectedCards.push(imageTiles[i]);
-
-    checkCards(selectedCards);
-  });
-}
-
+// addImageEventListener();
 let selectedCards: HTMLDivElement[] = [];
 function checkCards(selection: HTMLDivElement[]) {
   if (selection.length === 2) {
     if (selection[0].dataset.imgId === selection[1].dataset.imgId) {
-
-      document.getElementById(selection[0].id)!.style.visibility = "hidden";
-      document.getElementById(selection[1].id)!.style.visibility = "hidden";
+      (document.getElementById(selection[0].id) as HTMLDivElement).style.visibility = "hidden";
+      (document.getElementById(selection[1].id)as HTMLDivElement).style.visibility = "hidden";
 
       console.log("right choice");
-      imagesId=imagesId.filter((id)=>id!=Number(selection[0].dataset.imgId))
+      imagesId = imagesId.filter(
+        (id) => id != Number(selection[0].dataset.imgId)
+      );
       if (playerOne.playerStatus) updateLivescore(playerOne);
       else if (playerTwo.playerStatus) updateLivescore(playerTwo); //function to add score
     } else {
       console.log("wrong choice");
       playerChange(playerOne, playerTwo); //function to change turn
-      setTimeout(()=>{
-        selection[0].classList.toggle('rotated');
-        selection[1].classList.toggle('rotated');
-        selection[0]!.getElementsByTagName('img')[0].src="../assets/bg.png";
-        selection[1]!.getElementsByTagName('img')[0].src="../assets/bg.png";
-      },1000)
+      setTimeout(() => {
+        selection[0].classList.toggle("rotated");
+        selection[1].classList.toggle("rotated");
+        selection[0]!.getElementsByTagName("img")[0].src = "../assets/bg.png";
+        selection[1]!.getElementsByTagName("img")[0].src = "../assets/bg.png";
+      }, 1000);
     }
     selectedCards = [];
-    if(imagesId.length==0)
-      getWinner(playerOne,playerTwo)
+    if (imagesId.length == 0) getWinner(playerOne, playerTwo);
   }
 }
 
 
-
-
-let playerOne: PlayerType = {
-  id: 1,
-  playerName: "Player 1",
-  playerStatus: true,
-  playerScore: 0,
-};
-let playerTwo: PlayerType = {
-  id: 2,
-  playerName: "Player 2",
-  playerStatus: false,
-  playerScore: 0,
-};
 // change player
 const playerChange = (playerOne: PlayerType, playerTwo: PlayerType): void => {
   if (playerOne.playerStatus === true) {
@@ -157,7 +170,6 @@ const playerChange = (playerOne: PlayerType, playerTwo: PlayerType): void => {
     // clearInterval(playerInterval);
   }
 };
-
 
 // timer function
 // const playerInterval = setTimeout(playerChange, 1500);
@@ -183,23 +195,26 @@ function shuffleArray(array: any[]) {
   }
 }
 
-const getWinner = (player1: PlayerType, player2: PlayerType):PlayerType[] => {
+const getWinner = (player1: PlayerType, player2: PlayerType): PlayerType[] => {
   let winnerList: PlayerType[] = [];
-  if (player1.playerScore > player2.playerScore){
+  if (player1.playerScore > player2.playerScore) {
     winnerList.push(player1);
-    console.log(player1.playerName+ " Wins with score "+player1.playerScore);
-    
-  } 
-  else if (player1.playerScore < player2.playerScore) 
-    {
-      console.log(player2.playerName+ " Wins with score "+player2.playerScore);
-      winnerList.push(player2);
-    }
-  else {
+    console.log(player1.playerName + " Wins with score " + player1.playerScore);
+  } else if (player1.playerScore < player2.playerScore) {
+    console.log(player2.playerName + " Wins with score " + player2.playerScore);
+    winnerList.push(player2);
+  } else {
     console.log("Draw");
-    
+
     winnerList.push(player1);
     winnerList.push(player2);
   }
   return winnerList;
+};
+
+const categorySelection = (categoryName: string): void => {
+  console.log(categoryName);
+
+  loadCards(imageData, 8);
+  document.getElementById("category-section")!.style.display = "none";
 };
